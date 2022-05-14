e850885305153fef57f3a86a3b5536d4267174db
eduardito2009 <cm655176@gmail.com>
2022-05-14 12:03:35 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int num=1;
+        int  num2= 10;
+        System.out.println(num+num2);
+
 
     }
 
