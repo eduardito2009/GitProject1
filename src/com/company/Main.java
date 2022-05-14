6b0cfc70e1f1c33511da4006f5943341c1464019
eduardito2009 <cm655176@gmail.com>
2022-05-14 14:08:03 -0400

    update commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+       // Team lead project
         int num=1;
         int  num2= 10;
         System.out.println(num+num2);
@@ -9,6 +10,7 @@ public class Main {
         System.out.println(" Im correct code");
         System.out.println("I am not confused");
         System.out.println(" im confuse" );
+        System.out.println("hello friend");
 
 
 
